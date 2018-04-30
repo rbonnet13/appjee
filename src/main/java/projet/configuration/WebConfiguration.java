f3aa7cb084c9376eb84122fd73f541e2713be454
rbonnet13 <rbonnet13@users.noreply.github.com>
2018-04-30 15:23:55 +0200

    Update WebConfiguration.java

--- src/main/java/projet/configuration/WebConfiguration.java
+++ src/main/java/projet/configuration/WebConfiguration.java
@@ -1,16 +1,9 @@
 package projet.configuration;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfiguration {
-	@Bean
-	ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-		registrationBean.addUrlMappings("/h2-console/*");
-		return registrationBean;
-	}
 }
